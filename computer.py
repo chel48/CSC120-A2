b2d9faa152c6a67b9f984c2b8cd2107bdc841469
Chelsea Fowler <chelseafowler@Chelseas-MacBook-Air.local>
2022-09-20 20:48:54 -0400

    Added computer and resale classes

--- computer.py
+++ computer.py
@@ -1,10 +1,35 @@
-class Computer:
+"""
+creates a computer object with the properties of a computer
+"""
+class Computer: 
+    
+    """
+initializes the properties of the computer and makes them members of the object
+"""
+    def __init__(self, description: str,
+                    processor_type: str,
+                    hard_drive_capacity: int,
+                    memory: int,
+                    operating_system: str,
+                    year_made: int,
+                    price: int): 
+        self._description = description
+        self._processor_type = processor_type
+        self._hard_drive_capacity = hard_drive_capacity
+        self._memory = memory
+        self._operating_system = operating_system
+        self._year_made = year_made
+        self._price = price
 
-    # What attributes will it need?
-
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
-    # What methods will you need?
+    """
+creates a dictionary containing the properties of the computer
+"""
+    def _create_computer(self): 
+        return {'description': self._description,
+            'processor_type': self._processor_type,
+            'hard_drive_capacity': self._hard_drive_capacity,
+            'memory': self._memory,
+            'operating_system': self._operating_system,
+            'year_made': self._year_made,
+            'price': self._price
+    }
--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 from calendar import c
 from typing import Dict, Union
 
-# Import the functions we wrote in procedural_resale_shop.py
-from procedural_resale_shop import buy, update_price, sell, print_inventory, refurbish
+# Import the functions from computer.py and oo_resale_shop.py
+from computer import Computer
+from oo_resale_shop import ResaleShop
 
 """ This helper function takes in a bunch of information about a computer,
     and packages it up into a python dictionary to make it easier to store
@@ -13,30 +14,39 @@ from procedural_resale_shop import buy, update_price, sell, print_inventory, ref
     going to go the extra step, because when we get to Java it'll be required!
 """
 def create_computer(description: str,
-                    processor_type: str,
-                    hard_drive_capacity: int,
-                    memory: int,
-                    operating_system: str,
-                    year_made: int,
-                    price: int):
-    return {'description': description,
+                  processor_type: str,
+                     hard_drive_capacity: int,
+                     memory: int,
+                     operating_system: str,
+                     year_made: int,
+                     price: int):
+     return {'description': description,
             'processor_type': processor_type,
             'hard_drive_capacity': hard_drive_capacity,
-            'memory': memory,
-            'operating_system': operating_system,
-            'year_made': year_made,
-            'price': price
-    }
+             'memory': memory,
+             'operating_system': operating_system,
+             'year_made': year_made,
+             'price': price
+     }
 
 def main():
     
     # First, let's make a computer
-    computer = create_computer(
+    computer = Computer(
         "Mac Pro (Late 2013)",
         "3.5 GHc 6-Core Intel Xeon E5",
         1024, 64,
         "macOS Big Sur", 2013, 1500
     )
+    computer = computer._create_computer()
+    # print(computer)
+
+    computer2 = Computer("Mac Pro (Late 2014)", "4.5 GHc 6-Core Intel Xeon E5",
+    2048, 128, "macOS Monterey", 2014, 2000)
+    computer2 = computer2._create_computer()
+
+    #make the resale shop
+    resale_shop = ResaleShop()
 
     # Print a little banner
     print("-" * 21)
@@ -46,33 +56,49 @@ def main():
     # Add it to the resale store's inventory
     print("Buying", computer["description"])
     print("Adding to inventory...")
-    computer_id = buy(computer)
+    itemid = resale_shop.buy(computer)
+    print("Done.\n")
+
+    print("Buying", computer2["description"])
+    print("Adding to inventory...")
+    itemid2 = resale_shop.buy(computer2)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    resale_shop.print_inventory()
     print("Done.\n")
 
     # Now, let's refurbish it
     new_OS = "MacOS Monterey"
-    print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
+    print("Refurbishing Item ID:", itemid, ", updating OS to", new_OS)
     print("Updating inventory...")
-    refurbish(computer_id, new_OS)
+    resale_shop.refurbish(itemid, new_OS)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    resale_shop.print_inventory()
     print("Done.\n")
     
     # Now, let's sell it!
-    print("Selling Item ID:", computer_id)
-    sell(computer_id)
+    print("Selling Item ID:", itemid)
+    resale_shop.sell(itemid)
     
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    resale_shop.print_inventory()
+    print("Done.\n")
+
+    # Changes price of computer
+    new_price = 2500
+    print("Changing Price of Item", itemid2, "to", new_price)
+    resale_shop.update_price(itemid2, new_price)
+    print("Done.\n" )
+
+    # Make sure it worked by checking inventory
+    print("Checking inventory...")
+    resale_shop.print_inventory()
     print("Done.\n")
 
 # Calls the main() function when this file is run
